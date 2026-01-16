87520c45bca4df6cc19a61949b84b716788b6f98
Terenz Jahred Dantes <terenzdantes717@gmail.com>
2026-01-16 20:45:04 +0800

    feat: add loading state and spinner to MovieDetail component

--- src/pages/MovieDetail.jsx
+++ src/pages/MovieDetail.jsx
@@ -1,14 +1,18 @@
 import React from 'react'
 import { useParams } from 'react-router-dom'
 import { useState, useEffect } from 'react'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faSpinner } from '@fortawesome/free-solid-svg-icons';
 
 const MovieDetail = () => {
 
 const { id } = useParams()
 const [movie, setMovie] = useState([])
+const [loading, setLoading] = useState(false)
 
 useEffect(() => {
     async function fetchMovieDetail() {
+        setLoading(true)
         const API_KEY = import.meta.env.VITE_TMDB_API_KEY
         try {
             const res = await fetch(`https://api.themoviedb.org/3/movie/${id}?api_key=${API_KEY}`)
@@ -23,6 +27,8 @@ useEffect(() => {
             console.log(data)
         } catch (err) {
             console.log(err)
+        } finally {
+            setLoading(false)
         }
     }
 
@@ -32,17 +38,25 @@ useEffect(() => {
 
   return (
     <>
-        <div className='flex flex-col justify-center items-center border-1'>
-            <img src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}/>
-            <p>{movie.title}</p>
-            <p>{movie.release_date}</p>
-            <div>{movie.genres?.map((genre) => (
-                <span key={genre.id}>{genre.name} </span>
-            ))}</div>
-           
-            <p>Overview: {movie.overview}</p>
-            
+    <div className='h-screen flex justify-center items-center'>
+    {loading ? 
+        <div className='flex justify-center items-center w-full h-screen'>
+        <FontAwesomeIcon icon={faSpinner} className="animate-spin text-6xl" /> 
         </div>
+        : 
+        <div className='mx-50 flex flex-row justify-center items-center border-1 p-8 rounded-lg text-white h-auto bg-neutral-800 border-neutral-600'>
+            <img className='rounded-lg border-1 border-neutral-600' src={`https://image.tmdb.org/t/p/w200${movie.poster_path}`}/>
+            <div className='flex flex-col pl-8'>
+                <p className='text-3xl font-bold'>{movie.title}</p>
+                <p>Release Date: {movie.release_date}</p>
+                <div className='mt-4 mb-4'>{movie.genres?.map((genre) => (
+                    <span className="border-1 py-1 px-4 text-xs bg-neutral-700 border-neutral-500 rounded-lg mr-1" key={genre.id}>{genre.name} </span>
+                ))}</div>
+
+                <p>Overview: {movie.overview}</p>
+            </div>
+        </div>}
+    </div>
     </>
     
   )
